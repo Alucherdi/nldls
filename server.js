92a8cd6a32997461fbd4b789913f0b2823414402
Cesar Alejandro <cesaralejandro.perezconstantin@gmail.com>
2017-12-05 13:24:43 -0600

    Se modifica bd

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ var connection
 function handleDisconnect () {
 	connection = mysql.createConnection({
 		host: "50.62.209.199",
-		user: "cesarPromo2",
-		password: "c6drA^91",
-		database: "litros_suerte4"
+		user: "cesarPromo1",
+		password: "litrossuerte1994",
+		database: "Litros_suerte3"
 	})
 
 	connection.connect(function (err) {
